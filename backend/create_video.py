6f081493bdfde191ad1549cf37f25844555b9add
Freddie Lu <fredericklu@student.unsw.edu.au>
2024-12-07 15:17:21 +1100

    restructure save and create video into functions

--- backend/create_video.py
+++ backend/create_video.py
@@ -1,23 +1,24 @@
 import requests
 import json
 
-url = "https://tavusapi.com/v2/videos"
+def create_video(transcript):
+    url = "https://tavusapi.com/v2/videos"
+    payload = {
+        "background_url": "",
+        "replica_id": "rde3b1a18f",
+        "script": transcript,
+        "video_name": "reminder"
+    }
+    headers = {
+        "x-api-key": "4f0cf1d227a2478a89182fde9bddf8d3",
+        "Content-Type": "application/json"
+    }
 
-payload = {
-    "background_url": "",
-    "replica_id": "rde3b1a18f",
-    "script": "Hey Fred, get back to work!",
-    "video_name": "reminder"
-}
-headers = {
-    "x-api-key": "4f0cf1d227a2478a89182fde9bddf8d3",
-    "Content-Type": "application/json"
-}
+    response = json.loads(requests.request("POST", url, json=payload, headers=headers).text)
 
-response = json.loads(requests.request("POST", url, json=payload, headers=headers).text)
+    print(response)
 
-print(response)
-
-hosted_url = response["hosted_url"]
-video_id = response["video_id"]
-print(hosted_url)
+    hosted_url = response["hosted_url"]
+    video_id = response["video_id"]
+    print(hosted_url)
+    return [video_id, hosted_url]
